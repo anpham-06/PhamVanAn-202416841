eedb699539f7d7d784eab4d087c40a847294e7cf
anpham-06 <anphambg06@gmail.com>
2026-03-25 20:58:49 +0700

    Update bai63.java

--- Lab01/6.3/bai63.java
+++ Lab01/6.3/bai63.java
@@ -1,1 +1,26 @@
+import java.util.Scanner;
 
+public class bai63 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            // in khoảng trắng
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            // in dấu *
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println(); // xuống dòng
+        }
+
+        sc.close();
+    }
+}
