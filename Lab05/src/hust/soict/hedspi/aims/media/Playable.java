eba67cd62551d723bd70f31405ff208852676a01
An Pham <anphambg06@gmail.com>
2026-05-30 22:16:15 +0700

    addlab5

---
+++ Lab05/src/hust/soict/hedspi/aims/media/Playable.java
@@ -0,0 +1,9 @@
+/* Student: Pham Van An - 202416841
+ * Class: 761929
+ */
+package hust.soict.hedspi.aims.media;
+
+import hust.soict.hedspi.aims.exception.*;
+public interface Playable {
+	public void play() throws PlayerException;;
+}
